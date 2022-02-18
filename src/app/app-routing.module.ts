fce777f1b74d5fd4c5f023dfda893cc5e5473881
angelicaalbuquerque <angelica.o.albuquerque@gmail.com>
2022-02-18 15:47:59 -0300

    Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HeadernavComponent } from './shared/headernav/headernav.component';
 import { SidenavComponent } from './shared/sidenav/sidenav.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'sidenav', component: SidenavComponent},
-  {path: '', redirectTo:'/home', pathMatch: 'full'}
-]
+  { path: 'home', component: HomeComponent },
+  { path: 'sidenav', component: SidenavComponent },
+  { path: 'headernav', component: HeadernavComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { 
-  
-}
+export class AppRoutingModule {}
