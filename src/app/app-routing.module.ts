6276d7d5a219280a82fe684c6d08f61215933ddc
angelicaalbuquerque <angelica.o.albuquerque@gmail.com>
2022-02-18 11:35:11 -0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'sidenav', component: SidenavComponent},
+  {path: '', redirectTo:'/home', pathMatch: 'full'}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { HeadernavComponent } from './shared/headernav/headernav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidenavComponent,
+    HeadernavComponent
   ],
   imports: [
     BrowserModule,
